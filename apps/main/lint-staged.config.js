b4bcf25c14226d98e81077e7bb733a8e6db68aad
Nikita Yakunin <nayakunin99@gmail.com>
2025-10-24 12:47:58 +0200

    fix

--- apps/main/lint-staged.config.js
+++ apps/main/lint-staged.config.js
@@ -3,7 +3,7 @@ import micromatch from "micromatch";
 export default {
   // Lint then format TypeScript and JavaScript files
   "**/*.(ts|tsx|js|jsx|mjs)": (filenames) => {
-    const match = micromatch.not(filenames, "routeTree.gen.ts");
+    const match = filenames.filter((file) => file.includes("routeTree.gen.ts"));
     return [
       `npx eslint --fix ${match.join(" ")}`,
       `npx prettier --write ${match.join(" ")}`,
